83ad0783501f200a41578653bd61f36740d14003
drw <drw@users.noreply.github.com>
2019-06-18 13:59:05 -0400

    Standardize imports across two ETL jobs

--- engine/payload/ac_hd/geocode_restaurants.py
+++ engine/payload/ac_hd/geocode_restaurants.py
@@ -1,20 +1,16 @@
-import json
-import requests
-import datetime
-import csv
+import csv, json, requests, sys, traceback
 import time
 import re
-import sys, traceback
-
-from marshmallow import fields, pre_load, pre_dump
-
-sys.path.insert(0, '/Users/drw/WPRDC/etl-dev/wprdc-etl') # A path that we need to import code from
-import pipeline as pl # This comes from the wprdc-etl repository.
 from dateutil import parser
 
-from engine.parameters.local_parameters import SETTINGS_FILE, SOURCE_DIR, PRODUCTION
+from marshmallow import fields, pre_load, pre_dump
+sys.path.insert(0, '/Users/drw/WPRDC/etl-dev/wprdc-etl') # A path that we need to import code from
+import pipeline as pl # This comes from the wprdc-etl repository.
+
+from engine.parameters.local_parameters import SETTINGS_FILE, PRODUCTION
 from engine.parameters.remote_parameters import TEST_PACKAGE_ID
 from engine.etl_util import find_resource_id, post_process, local_file_and_dir
+from engine.notify import send_to_slack
 
 try:
     from icecream import ic
--- engine/payload/pgh/pli_violations_no_shell.py
+++ engine/payload/pgh/pli_violations_no_shell.py
@@ -1,18 +1,15 @@
-import requests
-import csv
-import json
-import datetime
-import sys, traceback
-from marshmallow import fields, pre_load, post_load
+import csv, json, requests, sys, traceback
+from datetime import datetime
 
-from credentials import API_key
+from marshmallow import fields, pre_load, post_load
 sys.path.insert(0, '/Users/drw/WPRDC/etl-dev/wprdc-etl') # A path that we need to import code from
 import pipeline as pl
 
-from parameters.local_parameters import SETTINGS_FILE, SOURCE_DIR, PRODUCTION
-from parameters.remote_parameters import TEST_PACKAGE_ID
-from etl_util import fetch_city_file, find_resource_id, post_process, local_file_and_dir
-from notify import send_to_slack
+from engine.credentials import API_key
+from engine.parameters.local_parameters import SETTINGS_FILE, PRODUCTION
+from engine.parameters.remote_parameters import TEST_PACKAGE_ID
+from engine.etl_util import find_resource_id, post_process, local_file_and_dir, fetch_city_file
+from engine.notify import send_to_slack
 
 CLEAR_FIRST = False
 
