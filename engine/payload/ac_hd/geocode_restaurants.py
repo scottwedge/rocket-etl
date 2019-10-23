246af1cfa7628abe751931bfa120913aec4212b1
drw <drw@users.noreply.github.com>
2019-10-23 13:59:34 -0400

    Bring geocode_restaurants up to date by switching to run_pipeline/locations_by_destination

--- engine/payload/ac_hd/geocode_restaurants.py
+++ engine/payload/ac_hd/geocode_restaurants.py
@@ -5,7 +5,7 @@ from dateutil import parser
 
 from marshmallow import fields, pre_load, pre_dump
 from engine.wprdc_etl import pipeline as pl
-from engine.etl_util import post_process, default_job_setup, push_to_datastore
+from engine.etl_util import post_process, default_job_setup, run_pipeline
 from engine.notify import send_to_slack
 
 try:
@@ -153,7 +153,7 @@ def process_job(**kwparameters):
     use_local_files = kwparameters['use_local_files']
     clear_first = kwparameters['clear_first']
     test_mode = kwparameters['test_mode']
-    target, local_directory, loader_config_string = default_job_setup(job)
+    target, local_directory, loader_config_string, destinations, destination_filepath, destination_directory = default_job_setup(job)
 
     ## BEGIN CUSTOMIZABLE SECTION ##
     file_connector = pl.FileConnector
@@ -166,5 +166,7 @@ def process_job(**kwparameters):
     upload_method = 'upsert'
     ## END CUSTOMIZABLE SECTION ##
 
-    resource_id = push_to_datastore(job, file_connector, target, config_string, encoding, loader_config_string, primary_key_fields, test_mode, clear_first, upload_method)
-    return [resource_id] # Return a complete list of resource IDs affected by this call to process_job.
+    locations_by_destination = run_pipeline(job, file_connector, target, config_string, encoding, loader_config_string, primary_key_fields, test_mode, clear_first, upload_method, destinations=destinations, destination_filepath=destination_filepath, file_format='csv')
+    # [ ] What is file_format used for? Should it be hard-coded?
+
+    return locations_by_destination # Return a dict allowing look up of final destinations of data (filepaths for local files and resource IDs for data sent to a CKAN instance).
