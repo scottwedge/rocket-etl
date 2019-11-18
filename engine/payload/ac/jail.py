b389ea3c3479ffbcda7c93b926d0d0257ad92038
drw <drw@users.noreply.github.com>
2019-11-18 15:31:08 -0500

    Fix order of arguments received from default_job_setup

--- engine/payload/ac/jail.py
+++ engine/payload/ac/jail.py
@@ -59,7 +59,8 @@ def process_job(**kwparameters):
     use_local_files = kwparameters['use_local_files']
     clear_first = kwparameters['clear_first']
     test_mode = kwparameters['test_mode']
-    target, local_directory, file_connector, local_cache_filepath, loader_config_string, destinations, destination_filepath, destination_directory = default_job_setup(job, use_local_files)
+    target, local_directory, local_cache_filepath, file_connector, loader_config_string, destinations, destination_filepath, destination_directory = default_job_setup(job, use_local_files)
+    ic(file_connector)
     ## BEGIN CUSTOMIZABLE SECTION ##
     config_string = ''
     encoding = 'utf-8'
