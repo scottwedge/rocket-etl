795dff0b3df6c8f49c1bc33f452a7cf3f4334eb6
drw <drw@users.noreply.github.com>
2020-03-02 15:19:08 -0500

    Add County park facilities assets

--- engine/payload/asset_map/_facet_hound.py
+++ engine/payload/asset_map/_facet_hound.py
@@ -1434,6 +1434,43 @@ class SchoolsSchema(pl.BaseSchema):
                         data['latitude'] = None
                         data['longitude'] = None
 
+class ParkFacilitiesSchema(pl.BaseSchema):
+    asset_type = fields.String(dump_only=True, default='parks')
+    name = fields.String(load_from='name', allow_none=False)
+    parent_location = fields.String(load_from='park', allow_none=True)
+    latitude = fields.Float(load_from='y', allow_none=True)
+    longitude = fields.Float(load_from='x', allow_none=True)
+    organization_name = fields.String(default='Allegheny County Parks Department')
+    #notes = fields.String(load_from='label', allow_none=True)
+    tags = fields.String(load_from='type', allow_none=True)
+    #additional_directions = fields.String(load_from='shopping_center', allow_none=True)
+    #url = fields.String(load_from='facility_u', allow_none=True)
+    #hours_of_operation = fields.String(load_from='day_time')
+    #child_friendly = fields.String(dump_only=True, allow_none=True, default=True)
+    #computers_available = fields.String(dump_only=True, allow_none=True, default=False)
+
+    #sensitive = fields.Boolean(dump_only=True, allow_none=True, default=False)
+    localizability = fields.String(dump_only=True, default='fixed')
+    # Include any of these or just leave them in the master table?
+    #date_entered = Leave blank.
+    #last_updated = # pull last_modified date from resource
+    data_source_name = fields.String(default='Allegheny County Park Facilities')
+    data_source_url = fields.String(default='https://data.wprdc.org/dataset/allegheny-county-park-facilities')
+
+    class Meta:
+        ordered = True
+
+    @pre_load
+    def fix_nones(self, data):
+        fs = ['name', 'park', 'type']
+        for f in fs:
+            if f in data and data[f] in [None, '', ' ']:
+                if f == 'name':
+                    data[f] = '(Unnamed location)'
+                else: # park and type (which maps to parent_location and tags)
+                    data[f] = None
+
+
 #def conditionally_get_city_files(job, **kwparameters):
 #    if not kwparameters['use_local_files']:
 #        fetch_city_file(job)
@@ -1935,6 +1972,19 @@ job_dicts = [
         # and could fail if multiple sources are combined.
         'destinations': ['file'],
         'destination_file': ASSET_MAP_PROCESSED_DIR + 'Public_and_Private_Education_Institutions_2017_Education_Allegheny-minus_PO_Boxes.csv'
+    },
+    {
+        'job_code': 'county_park_facilities',
+        'source_type': 'local',
+        'source_file': ASSET_MAP_SOURCE_DIR + 'Allegheny_County_Park_Facilities.csv',
+        'encoding': 'utf-8-sig',
+        #'custom_processing': conditionally_get_city_files,
+        'schema': ParkFacilitiesSchema,
+        'always_clear_first': True,
+        'primary_key_fields': ['objectid'], # These primary keys are really only primary keys for the source file
+        # and could fail if multiple sources are combined.
+        'destinations': ['file'],
+        'destination_file': ASSET_MAP_PROCESSED_DIR + 'Allegheny_County_Park_Facilities.csv',
     },
 ]
 # [ ] Fix fish-fries validation by googling for how to delete rows in marshmallow schemas (or else pre-process the rows somehow... load the whole thing into memory and filter).
