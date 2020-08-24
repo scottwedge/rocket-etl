33076468694729e45a5f99c46497246dd0c92862
drw <drw@users.noreply.github.com>
2020-08-23 20:56:24 -0400

    Add ETL job to obtain WPRDC monthly pageviews from Google Analytics

--- engine/payload/wprdc/g_analytics.py
+++ engine/payload/wprdc/g_analytics.py
@@ -1,7 +1,9 @@
 from oauth2client.service_account import ServiceAccountCredentials
 from apiclient.discovery import build
-import httplib2
-from collections import OrderedDict
+import httplib2, requests
+from collections import OrderedDict, defaultdict
+from datetime import date, timedelta, datetime
+import time
 
 from engine.parameters.google_api_credentials import profile
 #from google_api_credentials import profile
@@ -14,6 +16,11 @@ from engine.etl_util import Job
 from pprint import pprint
 from icecream import ic
 
+tracking_resource_id = None # There was originally a resource ID value
+# here, but I toggled it to None to switch off stuff that I decided to
+# stop doing in get_IDs. That stuff could probably be deleted at this
+# point.
+
 class WebStatsSchema(pl.BaseSchema):
     year_month = fields.String(load_from='Year+month'.lower(), dump_to='Year+month', allow_none=False)
     users = fields.Integer(load_from='Users', dump_to='Users', allow_none=False)
@@ -25,16 +32,129 @@ class WebStatsSchema(pl.BaseSchema):
     class Meta:
         ordered = True
 
-    @pre_load
-    def check(self, data):
-        ic(data)
+class MonthlyPageviewsSchema(pl.BaseSchema):
+    year_month = fields.String(load_from='Year+month'.lower(), dump_to='Year+month', allow_none=False)
+    package = fields.String(load_from='Package'.lower(), dump_to='Package', allow_none=False)
+    resource = fields.String(load_from='Resource'.lower(), dump_to='Resource', allow_none=False)
+    publisher = fields.String(load_from='Publisher'.lower(), dump_to='Publisher', allow_none=False)
+    groups = fields.String(load_from='Groups'.lower(), dump_to='Groups', allow_none=False)
+    package_id = fields.String(load_from='Package_ID'.lower(), dump_to='Package ID', allow_none=False)
+    resource_id = fields.String(load_from='Resource_ID'.lower(), dump_to='Resource ID', allow_none=False)
+    pageviews = fields.Integer(load_from='Pageviews', dump_to='Pageviews', allow_none=False)
+
+    class Meta:
+        ordered = True
+
+#    @pre_load
+#    def check(self, data):
+#        ic(data)
+
+def stringify_groups(p):
+    groups_string = ''
+    if 'groups' in p:
+        groups = p['groups']
+        groups_string = '|'.join(set([g['title'] for g in groups]))
+    return groups_string
+
+
+# Begin CKAN functions #
+def get_IDs():
+    # This function originally just got resource IDs (and other parameters) from the
+    # current_package_list_with_resources API endpoint. However, this ignores
+    # resources that existed but have been deleted (or turned private again). To
+    # track the statistics of these as well. We are now merging in historical
+    # resource IDs produced by dataset-tracker.
+    from engine.credentials import site
+    ckan_max_results = 9999999
+    resources, packages, = [], []
+    lookup_by_id = defaultdict(lambda: defaultdict(str))
+    url = "{}/api/3/action/current_package_list_with_resources?limit={}".format(site, ckan_max_results)
+    r = requests.get(url)
+    # Traverse package list to get resource IDs.
+    package_list = r.json()['result']
+    for p in package_list:
+        r_list = p['resources']
+        if len(r_list) > 0:
+            packages.append(r_list[0]['package_id'])
+            for k,resource in enumerate(r_list):
+                if 'id' in resource:
+                    resources.append(resource['id'])
+                    lookup_by_id[resource['id']]['package id'] = r_list[0]['package_id']
+                    lookup_by_id[resource['id']]['package name'] = p['title']
+                    lookup_by_id[resource['id']]['publisher'] = p['organization']['title']
+                    lookup_by_id[resource['id']]['groups'] = stringify_groups(p)
+                if 'name' in resource:
+                    lookup_by_id[resource['id']]['name'] = resource['name']
+#                else:
+#                    lookup_by_id[resource['id']]['name'] = 'Unnamed resource'
+
+    if tracking_resource_id is not None:
+        tracks = load_resource(site, tracking_resource_id, None)
+        for r in tracks:
+            r_id = r['resource_id']
+            if r_id not in resources:
+                if 'name' in resource:
+                    lookup_by_id[resource['id']]['name'] = resource['name']
+                    print("Adding resource ID {} ({})".format(r_id,r['resource_name']))
+                else:
+                    print("Adding resource ID {} (Unnamed resource)".format(r_id))
+                resources.append(r_id)
+                if r['package_id'] not in packages:
+                    packages.append(r['package_id'])
+                lookup_by_id[r_id]['package_id'] = r['package_id']
+                lookup_by_id[r_id]['package name'] = r['package_name']
+                lookup_by_id[r_id]['publisher'] = r['organization']
+                lookup_by_id[r_id]['groups'] = stringify_groups(p)
+
+    return resources, packages, lookup_by_id
+
+# End CKAN functions #
+
+def insert_zeros(rows, extra_columns, metrics_count, yearmonth='201510'):
+    # To make the data easier to work with, ensure that every dataset
+    # has the same number of month entries, even if a bunch of them
+    # are zero-downloads entries.
+    # yearmonth is the first year/month in the dataset.
+    first_year = int(yearmonth[0:4])
+    first_month = int(yearmonth[4:6])
+    today = date.today()
+    last_year = today.year
+    last_month = today.month
+
+    new_rows = []
+    year, month = first_year, first_month
+    while year <= last_year:
+        if year < last_year or month <= last_month:
+            current_ym = '{:d}{:02d}'.format(year,month)
+            yms = [row[0] for row in rows]
+            if current_ym in yms:
+                current_index = yms.index(current_ym)
+                new_rows.append(rows[current_index])
+            else:
+                row_of_zeros = [current_ym] + extra_columns
+                base_length = len(row_of_zeros)
+                for mtrc in range(0,metrics_count):
+                    row_of_zeros.append('0')
+                new_rows.append(row_of_zeros)
+        month += 1
+        if month == 13:
+            year += 1
+            month = 1
+        if month == last_month and year == last_year and today.day == 1:
+            year += 1 # Abort loop since Google Analytics has no data on
+            # the first of the month. This is a bit of a kluge, but it works.
+            # I am leaving this in to make the monthly-downloads sparklines
+            # consistent with the behavior of the Users plot on the front
+            # page of the dashboard.
+    return new_rows
+
 
 #https://developers.google.com/analytics/devguides/reporting/core/v4/migration#v4_14
 def initialize_ga_api():
     #Create service credentials
     #Rename your JSON key to client_secrets.json and save it to your working folder
     credentials = ServiceAccountCredentials.from_json_keyfile_name('analytics_credentials.json', ['https://www.googleapis.com/auth/analytics.readonly'])
-      
+
     #Create a service object
     http = credentials.authorize(httplib2.Http())
     service = build('analytics', 'v4', http=http, discoveryServiceUrl=('https://analyticsreporting.googleapis.com/$discovery/rest'))
@@ -135,8 +255,8 @@ def get_history_by_month(service,profile,metrics,resource_id=None,event=False):
         # has an eventCategory saying it is a download stat and
         # an eventLabel identifying a particular resource by ID.
         try:
-            requests = get_metrics(service, profile, metrics, start_date='2015-10-15', end_date='yesterday', 
-                    dimensions=[{"name": "ga:yearMonth"}, {"name": "ga:eventLabel"}, {"name": "ga:eventCategory"}], 
+            requests = get_metrics(service, profile, metrics, start_date='2015-10-15', end_date='yesterday',
+                    dimensions=[{"name": "ga:yearMonth"}, {"name": "ga:eventLabel"}, {"name": "ga:eventCategory"}],
                     sort_by=[{"fieldName": "ga:yearMonth", "sortOrder": "ASCENDING"}],
                     filters=f"ga:eventLabel=={resource_id}")
             #filters="ga:eventCategory==CKAN%20Resource%20Download%20Request;ga:eventLabel=="+resource_id)
@@ -159,7 +279,7 @@ def get_history_by_month(service,profile,metrics,resource_id=None,event=False):
                 for r in requests['data']['rows']:
                     if 'CKAN API Request' not in r:
                         v = requests['data']['rows']['metrics'][0]['values']
-                        requests['data']['rows']['metrics'][0]['values'] = v[:xi] + v[xi+1:] 
+                        requests['data']['rows']['metrics'][0]['values'] = v[:xi] + v[xi+1:]
 
            #'rows': [{'dimensions': ['201510'],
            #          'metrics': [{'values': ['1515',
@@ -172,7 +292,7 @@ def get_history_by_month(service,profile,metrics,resource_id=None,event=False):
            #                                  '1358',
            #                                  '13884',
            #                                  '10.223858615611192',
-           #                                  '333.00662739322536']}]}, 
+           #                                  '333.00662739322536']}]},
 
 #[{'columnHeader': {'dimensions': ['ga:yearMonth'],
 #'metricHeader': {'metricHeaderEntries': [{'name': 'ga:users',
@@ -187,6 +307,111 @@ def get_history_by_month(service,profile,metrics,resource_id=None,event=False):
 #             'type': 'TIME'}]}},
 
     return requests
+
+
+def fetch_and_store_metric(dmbm_file, metric, metrics_name, event, first_yearmonth, limit=0):
+    # target_resource_id is the resource ID of the dataset that the
+    # fetched information should be sent to (not to be confused with
+    # the resource IDs of the data files about which metrics are being
+    # obtained).
+    service = initialize_ga_api()
+    metrics = [{'expression': m} for m in metrics_name.keys()]
+
+    resources, packages, lookup_by_id = get_IDs()
+
+    #Write the field names as the first line of the file:
+    fcsv = open(dmbm_file, 'w')
+    extra_fields = ['Year+month']
+    extra_fields += ['Package', 'Resource', 'Publisher', 'Groups', 'Package ID', 'Resource ID']
+        # This is the first place to add extra fields.
+    #if metric == 'downloads':
+    #    extra_fields.remove("Resource ID") # This causes an error.
+    csv_row = ','.join(extra_fields + list(metrics_name.values()))
+    fcsv.write(csv_row+'\n')
+
+    all_rows = []
+    if limit > 0:
+        resources = resources[:limit]
+    for k, r_id in enumerate(resources):
+        metric_by_month = get_history_by_month(service, profile, metrics, r_id, event)
+        if metric_by_month is None:
+            print("Strike 1. ")
+            metric_by_month = get_history_by_month(service, profile, metrics, r_id, event)
+        if metric_by_month is None:
+            print("Strike 2. ")
+            time.sleep(1)
+            metric_by_month = get_history_by_month(service, profile, metrics, r_id, event)
+        if metric_by_month is None:
+            print("Strike 3. ")
+            raise Exception(f"Unable to get metric_by_month data for resource ID {r_id} after trying thrice.")
+
+
+        if 'reports' in metric_by_month and len(metric_by_month['reports']) > 0 and 'rows' in metric_by_month['reports'][0]['data']:
+            metric_rows = metric_by_month['reports'][0]['data']['rows']
+            # Unfortunately, Google Analytics does not provide the resource
+            # ID as a queryable parameter for pageviews or other metrics
+            # the way it does for downloads (since the resource ID has been
+            # inserted as the eventLabel).
+            # Therefore, I need to manually insert the resource ID in these
+            # cases (and now other parameters).
+
+            lbid = lookup_by_id[r_id]
+            new_metric_rows = []
+            for row in metric_rows:
+                if metric == 'downloads':
+                    row.remove(unicode(r_id))
+                new_metric_rows.append([row['dimensions'][0], lbid['package name'], lbid['name'], lbid['publisher'], lbid['groups'], lbid['package id'], r_id] + row['metrics'][0]['values'])
+                # This is the second place to add (and order) extra fields.
+            metric_rows = new_metric_rows
+
+            metric_rows = insert_zeros(metric_rows,
+                [lbid['package name'], lbid['name'], lbid['publisher'], lbid['groups'], lbid['package id'], r_id], len(metrics_name), first_yearmonth)
+                # This is the third place to add extra fields.
+
+            for row in metric_rows:
+                csv_row = ','.join(row)
+                fcsv = open(dmbm_file, 'a')
+                fcsv.write(csv_row+'\n')
+                fcsv.close()
+
+            all_rows += metric_rows
+        else:
+            print(f"No rows found in the response for the dataset with resource ID {r_id}")
+        time.sleep(1)
+
+# Create an update to the dataset-downloads dataset by just looking at this month and last month and upserting the results.
+#    if modify_datastore:
+#        # The fourth and final place to add extra fields is field_mapper,
+#        # which is now defined in pull_web_stats_from_ga, but you can also
+#        # just extend it here with a command like
+#        #       field_mapper['Beeblebrox'] = "dict"
+#        keys = ['Year+month', 'Resource ID']
+#        push_dataset_to_ckan(all_rows, metrics_name, server, target_resource_id, field_mapper, keys, extra_fields) #This pushes everything in metric_rows
+
+    fields = extra_fields + list(metrics_name.values())
+    return all_rows, fields
+
+    # [ ] Modify push_dataset_to_ckan to only initialize the datastore when necessary.
+        # This script could have two modes:
+        #   1) Download all data and overwrite the old stuff.
+        #   2) Download only this month and last month and upsert into
+        #   the existing repository.
+
+
+
+#    get_full_history(resource_id="40776043-ad00-40f5-9dc8-1fde865ff571")
+# Pull down daily downloads/unique downloads/pageviews and then monthly
+# stats, and then use the data.json file to filter down to the things
+# we want to track (maybe).
+
+# If we dump the output
+# u'rows': [[u'40776043-ad00-40f5-9dc8-1fde865ff571', u'668', u'260'],
+#       [u'7a417847-37bb-4a16-a25e-477f2a71661d', u'493', u'82'],
+#       [u'c0fcc09a-7ddc-4f79-a4c1-9542301ef9dd', u'139', u'78'],
+#
+# and prepend some kind of date information, that could essentially be the
+# stuff inserted into the dataset (once the types have been properly taken care of)
+
 
 # These two functions just illustrate the migration from v3 to v4:
 def download_monthly_stats_v3():
@@ -208,7 +433,7 @@ def download_monthly_stats_v4(service, profile, metrics, resource_id=None, event
                         'dateRanges': [{'startDate': '2015-10-15', 'endDate': 'today'}],
                         'metrics': metrics,
                         'dimensions': [{"name": "ga:yearMonth"}],
-        #                "filtersExpression":f"ga:pagePath={regex}", 
+        #                "filtersExpression":f"ga:pagePath={regex}",
                         'orderBys': [{"fieldName": "ga:yearMonth", "sortOrder": "DESCENDING"}],
                         'pageSize': 10000
                     }]
@@ -218,7 +443,7 @@ def download_monthly_stats_v4(service, profile, metrics, resource_id=None, event
     return requests
 # END functions for accessing Google Analytics #
 
-def write_to_csv(filename,list_of_dicts,keys):
+def write_to_csv(filename, list_of_dicts, keys):
     with open(filename, 'w') as output_file:
         dict_writer = csv.DictWriter(output_file, keys, extrasaction='ignore', lineterminator='\n')
         dict_writer.writeheader()
@@ -237,14 +462,11 @@ def pull_web_stats_from_ga(jobject, **kwparameters):
         metrics = [{'expression': m} for m in metrics_name.keys()]
 
         stats_by_month = get_history_by_month(service, profile, metrics)
-        ic(stats_by_month)
         if stats_by_month is None:
             stats_by_month = get_history_by_month(service, profile, metrics)
         if stats_by_month is None:
             raise Exception("Unable to get stats_by_month data after trying twice")
-        print(stats_by_month['reports'][0].keys())
         stats_rows = stats_by_month['reports'][0]['data']['rows']
-        pprint(stats_by_month['reports'][0]['data']['rows'])
 
         site_stats_file = jobject.local_cache_filepath
         #Write the field names as the first line of the file:
@@ -259,6 +481,14 @@ def pull_web_stats_from_ga(jobject, **kwparameters):
             fcsv.close()
         #time.sleep(0.5)
 
+def pull_monthly_pageviews_from_ga(jobject, **kwparameters):
+    metric = 'pageviews'
+    first_yearmonth = '201510'
+    event = False
+    metrics_name = OrderedDict([("ga:pageviews",'Pageviews')])
+
+    limit = 0
+    fetch_and_store_metric(jobject.local_cache_filepath, metric, metrics_name, event, first_yearmonth, limit)
 
 if __name__ == '__main__':
     pull_web_stats_from_ga(Job())
@@ -281,5 +511,21 @@ job_dicts = [
         'destination_file': f'web_stats.csv', # purposes.
         'package': analytics_package_id,
         'resource_name': f'WPRDC Site Statistics by Month'
+    },
+    {
+        'job_code': 'monthly_pageviews',
+        'source_type': 'local',
+        'source_dir': '',
+        'source_file': f'dataset_pageviews_by_month.csv',
+        'custom_processing': pull_monthly_pageviews_from_ga,
+        'encoding': 'utf-8-sig',
+        'schema': MonthlyPageviewsSchema,
+        'primary_key_fields': ['Year+month', 'Resource ID'],
+        'always_wipe_data': False,
+        'upload_method': 'upsert',
+        'destinations': ['file'], # These lines are just for testing
+        'destination_file': f'dataset_pageviews_by_month.csv', # purposes.
+        'package': analytics_package_id,
+        'resource_name': f'WPRDC Resource Pageviews by Month'
     },
 ]
